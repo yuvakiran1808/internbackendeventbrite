616686cdc43b582fecd9b443f39e3c1f5ab2a2f0
yuvakiran1808 <pyuvasaikiran@gmail.com>
2023-05-29 11:39:15 +0530

    final commit

--- Eventapp/settings.py
+++ Eventapp/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Eventapp.urls'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -139,3 +138,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
