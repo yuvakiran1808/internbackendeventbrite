3f7a1d355f6e122a26239b1d3bbe35083959ad34
yuvakiran1808 <pyuvasaikiran@gmail.com>
2023-05-29 12:03:47 +0530

    settings.py updated

--- Eventapp/urls.py
+++ Eventapp/urls.py
@@ -23,4 +23,3 @@ urlpatterns = [
     path('api/events/', EventListAPIView.as_view()),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
